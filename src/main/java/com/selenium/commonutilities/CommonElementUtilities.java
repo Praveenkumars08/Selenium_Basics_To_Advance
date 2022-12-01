a43735db4bebf94629f7995b75b57a58c75728c2
unknown <praveenkumar619.sp@gmail.com>
2022-12-01 14:42:27 +0530

    added common utilities

---
+++ src/main/java/com/selenium/commonutilities/CommonElementUtilities.java
@@ -0,0 +1,23 @@
+package com.selenium.commonutilities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class CommonElementUtilities {
+
+	private WebDriver driver;
+
+	public CommonElementUtilities(WebDriver driver) {
+		this.driver=driver;
+	}
+
+	public WebElement fElement(By locator) {
+		return driver.findElement(locator);
+	}
+
+	public void mySendKeys(By locator, String value) {
+		fElement(locator).sendKeys(value);
+	}
+
+}
