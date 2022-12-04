f92ef7c24ac24664a59b1bf9343b79970ea86830
unknown <praveenkumar619.sp@gmail.com>
2022-12-04 18:25:12 +0530

    added example code to print footer links

--- src/main/java/com/selenium/basics/Basics15.java
+++ src/main/java/com/selenium/basics/Basics15.java
@@ -10,20 +10,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Basics15 {
-	
+
 	public static void main(String[] args) {
-		
+
 		/**
 		 * Printing all the footer links from amazon.
 		 */
-		
+
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.amazon.in/");
-		
+
 		List<WebElement> footerLinks = driver.findElements(By.xpath("//div[@role='presentation']//div/div//ul/li"));
-		
+
 		for(WebElement e : footerLinks) {
 			String txt = e.getText();
 			System.out.println(txt);
