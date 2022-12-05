c2f4552bfeb03c9603afc174c4d404e594fe0da4
unknown <praveenkumar619.sp@gmail.com>
2022-12-05 15:44:12 +0530

    added code to handle iframes

--- src/main/java/com/selenium/intermediate/Intermediate_01_HandlingIFrames.java
+++ src/main/java/com/selenium/intermediate/Intermediate_01_HandlingIFrames.java
@@ -8,19 +8,28 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Intermediate_01_HandlingIFrames {
-	
+
 	public static void main(String[] args) {
 		
+		/**
+		 * Frames cannot be handled directly, we need to switch inside to the frame.
+		 * driver.switchTo().frame() -> frame() is a overloaded method it accepts parameters as index, id or name, and WebElement
+		 * While we are inside the frame, we can't access the outside of the main page, so again we need to switch back to defaultContent
+		 * NOTE :- We can also have multiple frames available in the Web Page.
+		 */
+
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		
+
 		driver.get("https://the-internet.herokuapp.com/iframe");
-		
+
 		driver.switchTo().frame("mce_0_ifr");
-		
+
 		WebElement frame = driver.findElement(By.id("tinymce"));
 		frame.sendKeys("Hello");
+
+		driver.switchTo().defaultContent();
 	}
 
 }
