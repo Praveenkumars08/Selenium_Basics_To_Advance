3a0e939907e37cfc564caa1bb78549d9d94c4404
unknown <praveenkumar619.sp@gmail.com>
2022-12-07 16:56:16 +0530

    added code to handle web table

---
+++ src/main/java/com/selenium/intermediate/Intermediate_05_HandlingWebTableForSpecificUser.java
@@ -0,0 +1,40 @@
+package com.selenium.intermediate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Intermediate_05_HandlingWebTableForSpecificUser {
+	
+	public static WebDriver driver;
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://selectorshub.com/xpath-practice-page/");
+		Thread.sleep(3000);
+		
+		List<String> uList = getUserData("Kevin.Mathews");
+		System.out.println(uList);
+	}
+	
+	public static List<String> getUserData(String plsPassExactUserName) {
+		List<WebElement> userList = driver.findElements(By.xpath("//table[@id='resultTable']//td/input/parent::td/following-sibling::td/a[text()='"+plsPassExactUserName+"']/parent::td/following-sibling::td"));
+		ArrayList<String> ar = new ArrayList<>();
+		for(WebElement e : userList) {
+			String txt = e.getText();
+			ar.add(txt);
+		}
+		return ar;
+	}
+
+}
