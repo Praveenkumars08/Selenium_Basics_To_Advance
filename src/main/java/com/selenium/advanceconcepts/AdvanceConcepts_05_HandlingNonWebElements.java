c87ece1f629d1fc495d361c2f068595d14140ae5
unknown <praveenkumar619.sp@gmail.com>
2022-12-12 17:28:09 +0530

    added code to handle non webelements using explicit wait

---
+++ src/main/java/com/selenium/advanceconcepts/AdvanceConcepts_05_HandlingNonWebElements.java
@@ -0,0 +1,39 @@
+package com.selenium.advanceconcepts;
+
+import java.time.Duration;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class AdvanceConcepts_05_HandlingNonWebElements {
+
+	static WebDriver driver;
+
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://nxtgenaiacademy.com/alertandpopup/");
+		driver.findElement(By.name("alertbox")).click();
+		Alert alert = waitForAlert(5);
+		alert.accept();
+
+	}
+
+	public static Alert waitForAlert(int timeOut) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeOut));
+		return wait.until(ExpectedConditions.alertIsPresent());
+	}
+
+	public static String WaitForAlertGetText(int timeOut) {
+		return waitForAlert(timeOut).getText();
+	}
+
+}
